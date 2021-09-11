27df04066b34184ce2560ada37d0f90da19ac8e6
Arina <awwwrina@gmail.com>
2021-09-11 20:44:16 +0300

    portfolio(mob.ver.)

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ const hamburger = document.querySelector('.hamburger');
 const overlay = document.querySelector('.menu__overlay');
 const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
+const menuLink = document.querySelector('.menu__link');
 
 const closeHandler = () => {
     menu.classList.remove('active');
@@ -15,6 +16,8 @@ menu.classList.add('active');
 closeElem.addEventListener('click', closeHandler);
 
 overlay.addEventListener('click', closeHandler);
+
+menuLink.addEventListener('click', closeHandler);
 
 const counters = document.querySelectorAll('.skills__ratings-counter'),
       lines = document.querySelectorAll('.skills__ratings-line span');
